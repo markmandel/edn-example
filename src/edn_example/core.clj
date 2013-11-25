14c87246ac1f46025808b41a59d8c07c78147466
Mark Mandel <mark.mandel@gmail.com>
2013-11-26 09:27:32 +1100

    Converting a Goat back from EDN.

--- src/edn_example/core.clj
+++ src/edn_example/core.clj
@@ -22,6 +22,8 @@
     Object
     ;; We overwrite toString for this defrecord to keep up with (str ...) being
     ;; able to output EDN directly.
+    ;; We will make '#edn-example.Goat' out tag, and then use the stardart toString
+    ;; for a Clojure map to do the rest.
     (toString [this] (str "#edn-example.Goat" (into {} this))))
 
 (def sample-goat (->Goat "I love Goats", "Goats are awesome"))
@@ -38,6 +40,15 @@
         (edn/read-string (convert-sample-goat-to-edn))
         (catch Exception e (str "Caught Exception: " (.getMessage e)))))
 
+;; This is a map of reader functions that match up to the #tags we have.
+;; we can use map->Goat, as we get back a map from the EDN block after the tag
+;; deserialises as a map, and we can just pass that through.
+(def edn-readers {'edn-example.Goat map->Goat})
+
+(defn convert-edn-to-goat
+    []
+    (edn/read-string {:readers edn-readers} (convert-sample-goat-to-edn)))
+
 (defn -main
     "Show off the EDN examples"
     [& args]
@@ -48,7 +59,8 @@
     (print "Now let's covert the vector back: ")
     (println (edn/read-string (convert-sample-vector-to-edn)))
     (println "Let's convert our defrecord Goat into EDN: " (convert-sample-goat-to-edn))
-    (println "Let's try converting a Goat back to EDN, but it will fail: " (fail-converting-edn-to-goat)))
+    (println "Let's try converting a Goat back to EDN, but it will fail: " (fail-converting-edn-to-goat))
+    (println "Let's try converting a Goat back to EDN: " (convert-edn-to-goat)))
 
 
 
